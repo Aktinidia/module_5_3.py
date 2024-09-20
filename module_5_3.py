186b5a0d6897107cbfb61250a6309779ad54a666
Aktinidia <nata_kul@mail.ru>
2024-09-20 15:39:33 +0300

    Add files via upload

--- module_5_3.py
+++ module_5_3.py
@@ -14,7 +14,7 @@ class House:
         print(self.number_of_floors)
 
     def __str__(self):
-        print("Название: ", self.name, "кол-во этажей: ", self.number_of_floors)
+        print("Название: ", self.name, ". Kол-во этажей: ", self.number_of_floors)
 
     def __eq__(self, other):
         if isinstance(other, House):
@@ -22,12 +22,56 @@ class House:
         elif isinstance(other, int):
             return self.number_of_floors == other
 
+    def __lt__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors < other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors < other
+
+    def __le__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors <= other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors <= other
+
+    def __gt__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors > other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors > other
+
+    def __ge__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors >= other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors >= other
+
+    def __ne__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors != other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors != other
+
+    def __add__(self, value):
+        if isinstance(value, House):
+            self.number_of_floors += value.number_of_floors
+            return self
+        elif isinstance(value, int):
+            self.number_of_floors += value
+            return self
+
 
 h1 = House('ЖК Эльбрус', 10)
 h2 = House('ЖК Акация', 20)
 
 
 # __str__
-h1.__eq__(10)
-h2.__eq__(20)
-
+print(h1.__eq__(11))
+print(h2.__eq__(20))
+print(h1.__lt__(13))
+print(h1.__le__(13))
+print(h1.__gt__(13))
+print(h1.__ge__(13))
+print(h1.__ne__(13))
+print(h2.__add__(14))
+print(h2.__add__(10))
